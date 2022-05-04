009a3a616a5319a65fe94af54732981a725b38c5
hiro-minami <hirotaka.minamida@resily.com>
2022-05-04 21:41:02 +0900

    homebrew対応

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,13 @@
-#view
+# homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# view
 set -g theme_display_date yes
 set -g theme_date_format "+%F %H:%M"
 set -g theme_display_git_default_branch yes
 set -g theme_color_scheme dark
 
-#path
+# path
 set -x PATH $HOME/.nodebrew/current/bin $PATH
 
 set -x PYENV_ROOT $HOME/.pyenv
