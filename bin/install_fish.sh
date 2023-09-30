30e0c0edc9e42b4f410038a59b4661df37e97d80
hiro-minami <hiro.0907@icloud.com>
2023-09-29 21:32:13 -0700

    🐛fish周りの修正

--- bin/install_fish.sh
+++ bin/install_fish.sh
@@ -14,3 +14,23 @@ git clone https://github.com/powerline/fonts.git
 cd fonts
 ./install.sh
 rm -rf ./fonts
+
+# fish-bd
+fisher install 0rax/fish-bd
+
+# z
+brew install z
+fisher install jethrokuan/z
+
+# peco
+brew install peco
+fisher install oh-my-fish/plugin-peco
+
+# fzf
+brew install fzf
+fisher install jethrokuan/fzf
+
+# ghq
+brew install ghq
+git config --global ghq.root ~/git
+fisher install decors/fish-ghq
