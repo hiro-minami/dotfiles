e2cac763477b9c433c8a23204c8c64ab5f0e8766
hiro-minami <hiro.0907@icloud.com>
2026-01-30 23:31:38 +0900

    スクリプトを修正

--- bin/install_fish.sh
+++ bin/install_fish.sh
@@ -1,39 +1,59 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+DOTFILES_DIR="$(dirname "$SCRIPT_DIR")"
+
 # fish
 brew install fish
-rm -r ~/.config/fish
-ln -sf ~/dotfiles/fish ~/.config
-sudo sh -c 'echo $(which fish) >> /etc/shells'
-chsh -s $(which fish)
 
-# fonts
+# Add fish to /etc/shells if not already present
+if ! grep -q "$(which fish)" /etc/shells; then
+    sudo sh -c 'echo $(which fish) >> /etc/shells'
+fi
+
+# Change default shell to fish
+chsh -s "$(which fish)"
+
+# Link fish config
+rm -rf ~/.config/fish
+ln -sf "$DOTFILES_DIR/fish" ~/.config/fish
+
+# Install fisher (fish plugin manager)
 brew install fisher
-fisher install oh-my-fish/theme-bobthefish
-git clone https://github.com/powerline/fonts.git
-cd fonts
-./install.sh
-rm -rf ./fonts
 
-# fish-bd
-fisher install 0rax/fish-bd
-
-# z
+# Install brew packages first (before changing directories)
 brew install z
-fisher install jethrokuan/z
-
-# peco
 brew install peco
-fisher install oh-my-fish/plugin-peco
-
-# fzf
 brew install fzf
-fisher install jethrokuan/fzf
-
-# ghq
 brew install ghq
-git config --global ghq.root ~/git
-fisher install decors/fish-ghq
 
-# fish_config
-mv ../fish/config.fish ../fish/fish/config.fish
+# Set ghq root
+git config --global ghq.root ~/git
+
+# Install Powerline fonts (use subshell to preserve current directory)
+(
+    rm -rf /tmp/powerline-fonts
+    git clone https://github.com/powerline/fonts.git /tmp/powerline-fonts
+    cd /tmp/powerline-fonts && ./install.sh
+    rm -rf /tmp/powerline-fonts
+)
+
+# Install fisher plugins (must be run inside fish shell)
+fish -c '
+    # Install fisher if not installed
+    if not functions -q fisher
+        curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
+        fisher install jorgebucaran/fisher
+    end
+
+    # Install plugins
+    fisher install oh-my-fish/theme-bobthefish
+    fisher install 0rax/fish-bd
+    fisher install jethrokuan/z
+    fisher install oh-my-fish/plugin-peco
+    fisher install jethrokuan/fzf
+    fisher install decors/fish-ghq
+'
+
+echo "✅ Fish shell setup complete!"
+echo "Please restart your terminal or run 'exec fish' to start using fish shell."
